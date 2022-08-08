8493341a6cff5095a6f7226c61f77c6805caf6a6
jayanth336 <jayanthram1320@gmail.com>
2022-08-08 15:28:45 +0530

    intial commit

---
+++ SpringWithThymeleaf/src/main/java/com/application/SpringWithThymeleaf/SpringWithThymeleafApplication.java
@@ -0,0 +1,14 @@
+package com.application.SpringWithThymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithThymeleafApplication.class, args);
+		System.out.println("`````````````````Application Started`````````````````");
+	}
+
+}
