bf5849a3885fafaa008c3c95b6631f1f61559139
Jayanth <110449215+jayanth336@users.noreply.github.com>
2023-01-01 22:20:04 +0530

    Delete SpringBoot-Crud-Jpa-H2db directory

--- SpringBoot-Crud-Jpa-H2db/src/main/java/com/application/SpringBootCrudJpaH2db/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.application.SpringBootCrudJpaH2db.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.application.SpringBootCrudJpaH2db.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-}
